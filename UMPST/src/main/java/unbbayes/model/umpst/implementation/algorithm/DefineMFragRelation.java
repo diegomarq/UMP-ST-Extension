167fb9abc28c24df63c50a8579985f67723e5d97
diegomarq <diegomarques.azevedo@gmail.com>
2016-05-07 19:03:29 -0300

    Graduation Work code. All definition of rules and first-criterion-selection are implemented. Second-criterion-seleciton is in implementing phase.

---
+++ UMPST/src/main/java/unbbayes/model/umpst/implementation/algorithm/DefineMFragRelation.java
@@ -0,0 +1,421 @@
+package unbbayes.model.umpst.implementation.algorithm;
+
+import java.awt.print.Printable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import unbbayes.controller.umpst.GenerateMTheoryController;
+import unbbayes.model.umpst.entity.RelationshipModel;
+import unbbayes.model.umpst.group.GroupModel;
+import unbbayes.model.umpst.implementation.CauseVariableModel;
+import unbbayes.model.umpst.implementation.EffectVariableModel;
+import unbbayes.model.umpst.implementation.EnumType;
+import unbbayes.model.umpst.implementation.EventNCPointer;
+import unbbayes.model.umpst.implementation.NecessaryConditionVariableModel;
+import unbbayes.model.umpst.implementation.NodeFormulaTree;
+import unbbayes.model.umpst.implementation.node.NodeInputModel;
+import unbbayes.model.umpst.implementation.node.NodeObjectModel;
+import unbbayes.model.umpst.implementation.node.NodeResidentModel;
+import unbbayes.model.umpst.implementation.node.NodeType;
+import unbbayes.model.umpst.project.UMPSTProject;
+import unbbayes.model.umpst.rule.RuleModel;
+
+/**
+ * This class deals with causal relation related to rule. This rule is in a group.
+ * @param rule
+ * @param group
+ */
+public class DefineMFragRelation {
+	
+	private RuleModel rule;
+	private GroupModel group;
+	private EffectVariableModel effectWasCause;
+	private Map<String, GroupModel> mapGroup;
+	
+	private MFragModel mfragSelected;
+	private NodeObjectModel nodeFather;
+	private NodeObjectModel notDefinedNode;
+//	private NodeResidentModel residentNodeFather;
+	
+	private GenerateMTheoryController generateMTheoryController;
+	private SecondCriterionOfSelection secondCriterion;
+	
+	private UMPSTProject umpstProject;
+	
+	public DefineMFragRelation(RuleModel rule, GroupModel group, GenerateMTheoryController generateMTheoryController, 
+			UMPSTProject umpstProject, SecondCriterionOfSelection secondCriterion) {
+		
+		this.rule = rule;
+		this.group = group;
+		this.generateMTheoryController = generateMTheoryController;
+		this.umpstProject = umpstProject;
+		this.secondCriterion = secondCriterion;
+		
+		selectMFragRelated();
+		mapCausalRelation();
+		mapMissingRelationships();
+	}
+	
+	public void mapMissingRelationships() {
+		List<RelationshipModel> listMissingRelationship = searchMissingRelationship();
+		
+//		for (int i = 0; i < listMissingRelationship.size(); i++) {
+//			System.out.println(listMissingRelationship.get(i).getName());
+//		}		
+	}
+	
+	/**
+	 * Verify if there are relationships that were not defined in rule definition as cause or effect.
+	 * If it exist, then it is added in list of missing relationships.
+	 * @return List<RelationshipModel>
+	 */
+	public List<RelationshipModel> searchMissingRelationship() {
+		List<RelationshipModel> listMissingRelationship = new ArrayList<RelationshipModel>();
+		boolean exist;
+		
+		for (int i = 0; i < group.getBacktrackingRelationship().size(); i++) {
+			RelationshipModel relationshipOfGroup = group.
+					getBacktrackingRelationship().get(i);			
+			exist = false;
+			
+			for (int j = 0; j < rule.getCauseVariableList().size(); j++) {
+				CauseVariableModel causeVar = rule.getCauseVariableList().get(j);				
+				RelationshipModel relationshipOfCause = causeVar.getRelationshipModel();
+				
+				if (relationshipOfGroup.equals(relationshipOfCause)) {
+					exist = true;
+					break;
+				}
+			}
+			for (int j = 0; j < rule.getEffectVariableList().size(); j++) {
+				EffectVariableModel effectVar = rule.getEffectVariableList().get(j);				
+				RelationshipModel relationshipOfEffect = effectVar.getRelationshipModel();
+				
+				if (relationshipOfGroup.equals(relationshipOfEffect)) {
+					exist = true;
+					break;
+				}
+			}
+			
+			// TODO recognize structures like isProcurementFinished(proc)^(enterprise = hasWinnerOfProcurement(proc))
+			exist = existsAsNecessaryCondition(relationshipOfGroup);
+				
+			
+//			for (int j = 0; j < rule.getRelationshipList().size(); j++) {								
+//				RelationshipModel relationshipOfRule = rule.getRelationshipList().get(j);
+//				
+//				if (relationshipOfGroup.equals(relationshipOfRule)) {
+//					exist = true;
+//					break;
+//				}
+//			}
+			if (!exist) {
+//				System.out.println("--"+group.getName()+"--");
+//				System.out.println(relationshipOfGroup.getName());
+				listMissingRelationship.add(relationshipOfGroup);
+			}
+		}
+		return listMissingRelationship;
+	}
+	
+	// The algorithm maps all relationship as resident node at some MFrag related to group.
+	public boolean existsAsNecessaryCondition(RelationshipModel relationship) {
+		boolean exists = false;		
+		for (int j = 0; j < rule.getNecessaryConditionList().size(); j++) {
+			NecessaryConditionVariableModel nc = rule.getNecessaryConditionList().get(j);			
+			NodeFormulaTree formulaTree = nc.getFormulaTree();
+			
+			exists = compareNodeFormula(formulaTree, relationship);
+			if (exists) {
+				System.out.println("--"+group.getName()+"--");
+				System.out.println(relationship.getName());
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean compareNodeFormula(NodeFormulaTree node, RelationshipModel relationship) {
+		if (node.getTypeNode() == EnumType.OPERAND) {
+			EventNCPointer event = (EventNCPointer)node.getNodeVariable();
+			RelationshipModel ncRelationship = event.getEventVariable().getRelationshipModel();
+			if (ncRelationship.equals(relationship)) {
+				return true;
+			}
+			if (node.getChildren().size() > 0) {
+				compareNodeFormula(node.getChildren().get(0), relationship);			
+			}
+		}
+		return false;
+	}
+		
+	public void mapCausalRelation() {
+		for (int j = 0; j < rule.getCauseVariableList().size(); j++) {				
+			CauseVariableModel cause = rule.getCauseVariableList().get(j);
+
+			// Cause was mapped as resident node in first criterion of selection
+			NodeResidentModel residentNode = searchResidentNode(cause);
+			
+			if (residentNode != null) {
+				nodeFather = residentNode;
+				mapCauseAsResident(cause);
+				mapAllEffectsAsResident();
+			}
+			// Cause is Effect in other rule
+			else if(existsAsEffect(cause)) {
+				mapCauseAsInput(cause);
+				mapAllEffectsAsResident();
+			}
+			// Verify if there are nodes that were not mapped. 
+			// Usually, these are nodes that were not mapped in other rule as effect.
+			else if(searchInputNode(cause) == null) {
+				String key = mfragSelected.getId();				
+				mapCauseAsNotDefined(cause);
+				
+				secondCriterion.getMapDoubtNodes().put(key, notDefinedNode);
+			}
+		}
+	}
+	
+	public void mapCauseAsNotDefined(CauseVariableModel cause) {
+		String sentence = cause.getRelationship() + "(";
+		for (int k = 0; k < cause.getArgumentList().size(); k++) {				
+			sentence = sentence + cause.getArgumentList().get(k) + ", ";
+		}
+		int index = sentence.lastIndexOf(", ");
+		sentence = sentence.substring(0, index);
+		sentence = sentence + ")";
+		
+		String id = cause.getId();
+		notDefinedNode = new NodeInputModel(id, sentence, NodeType.NOT_DEFINED, cause);
+	}
+	
+	
+	/**
+	 * Verify if there are rules which have cause relationship as effect and map as input node if it exists. 
+	 * @param cause
+	 */
+	public boolean existsAsEffect(CauseVariableModel cause) {
+		
+		mapGroup = umpstProject.getMapGroups();
+		Set<String> keys = mapGroup.keySet();
+		TreeSet<String> sortedKeys = new TreeSet<String>(keys);
+		
+		for (String key : sortedKeys) {
+			GroupModel groupSearched = mapGroup.get(key);
+			
+			if (!(group.getId().equals(groupSearched.getId()))) {
+				
+				for (int i = 0; i < groupSearched.getBacktrackingRelationship().size(); i++) {
+					RelationshipModel relationship = groupSearched.
+							getBacktrackingRelationship().get(i);
+					
+					if (relationship.getName().equals(cause.getRelationship())) {
+						
+						RuleModel ruleSelected = selectRuleRelated(groupSearched, cause);
+						if (ruleSelected != null) {
+							return true;
+						}
+					}
+				}			
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Select rule that defines as effect the cause passed as argument.
+	 * @param groupSearched
+	 * @param cause
+	 * @return RuleModel
+	 */
+	public RuleModel selectRuleRelated(GroupModel groupSearched, CauseVariableModel cause) {
+		for (int i = 0; i < groupSearched.getBacktrackingRules().size(); i++) {
+			RuleModel rule = groupSearched.getBacktrackingRules().get(i);
+			EffectVariableModel effect = searchEffect(rule, cause);
+			if (effect != null) {
+				return rule;
+			}		
+		}
+		return null;
+	}
+	
+	/**
+	 * Verify if rule defines cause selected as effect and returns the effect related. If rule defines, then
+	 * set as resident node the relationship defined as effect.
+	 * @param rule
+	 * @param cause
+	 * @return effect
+	 */
+	public EffectVariableModel searchEffect(RuleModel rule, CauseVariableModel cause) {
+		for (int i = 0; i < rule.getEffectVariableList().size(); i++) {
+			EffectVariableModel effect = rule.getEffectVariableList().get(i);
+			if (effect.getRelationshipModel().equals(cause.getRelationshipModel())) {
+//				mapEffectAsResident(effect);
+				return effect;
+			}
+		}
+		return null;
+	}
+	
+//	public void mapEffectAsResident(EffectVariableModel effect) {
+//		String sentence = effect.getRelationship() + "(";
+//		for (int k = 0; k < effect.getArgumentList().size(); k++) {				
+//			sentence = sentence + effect.getArgumentList().get(k) + ", ";
+//		}
+//		int index = sentence.lastIndexOf(", ");
+//		sentence = sentence.substring(0, index);
+//		sentence = sentence + ")";
+//		
+//		residentNodeFather.setName(sentence);
+//		residentNodeFather.setNodeType(NodeType.RESIDENT);
+//		residentNodeFather.setEventVariable(effect);
+//	}
+	
+	/**
+	 * Map all effects as resident node and set as father node the node mapped in cause relation.
+	 * On this method residentNode
+	 */
+	public void mapAllEffectsAsResident() {
+		for (int l = 0; l < rule.getEffectVariableList().size(); l++) {
+			EffectVariableModel effect = rule.getEffectVariableList().get(l);
+			String id = effect.getId();
+						
+			String sentence = effect.getRelationship() + "(";
+			for (int k = 0; k < effect.getArgumentList().size(); k++) {				
+				sentence = sentence + effect.getArgumentList().get(k) + ", ";
+			}
+			int indexChild = sentence.lastIndexOf(", ");
+			sentence = sentence.substring(0, indexChild);
+			sentence = sentence + ")";
+			
+			NodeResidentModel residentNodeChild = new NodeResidentModel(id, sentence, 
+					NodeType.RESIDENT, effect);
+			residentNodeChild.addFatherNode(nodeFather);
+			nodeFather.addChildrenNode(residentNodeChild);
+			
+			generateMTheoryController.addResidentNodeInMFrag(group.getId(), residentNodeChild);
+		}
+		if (nodeFather.getNodeType() == NodeType.RESIDENT_CAUSE) {
+			generateMTheoryController.updateResidentNodeInMFrag(group.getId(), (NodeResidentModel)nodeFather);
+		}
+		else if (nodeFather.getNodeType() == NodeType.INPUT) {
+			generateMTheoryController.updateInputNodeInMFrag(group.getId(), (NodeInputModel)nodeFather);
+		}
+	}
+	
+	public void mapCauseAsInput(CauseVariableModel cause) {
+		String sentence = cause.getRelationship() + "(";
+		for (int k = 0; k < cause.getArgumentList().size(); k++) {				
+			sentence = sentence + cause.getArgumentList().get(k) + ", ";
+		}
+		int index = sentence.lastIndexOf(", ");
+		sentence = sentence.substring(0, index);
+		sentence = sentence + ")";
+		
+		String id = cause.getId();
+		nodeFather = new NodeInputModel(id, sentence, NodeType.INPUT, cause);
+	}
+	
+	public void mapCauseAsResident(CauseVariableModel cause) {
+		String sentence = cause.getRelationship() + "(";
+		for (int k = 0; k < cause.getArgumentList().size(); k++) {				
+			sentence = sentence + cause.getArgumentList().get(k) + ", ";
+		}
+		int index = sentence.lastIndexOf(", ");
+		sentence = sentence.substring(0, index);
+		sentence = sentence + ")";
+		
+		if (nodeFather != null) {
+			nodeFather.setName(sentence);
+			nodeFather.setNodeType(NodeType.RESIDENT_CAUSE);
+			nodeFather.setEventVariable(cause);
+		} else {
+			String id = cause.getId();			
+			NodeResidentModel residentNode = new NodeResidentModel(id, sentence,
+					NodeType.RESIDENT_CAUSE, cause);
+			nodeFather = residentNode;
+		}
+		
+	}
+	
+	/**
+	 * Verify if cause relationship defined in rule was mapped as 
+	 * resident node in MFrag related to rule. If it is, then return
+	 * resident node identified.
+	 * @param cause
+	 * @return residentNode
+	 */
+	public NodeResidentModel searchResidentNode(CauseVariableModel cause) {
+		
+		// Resident nodes selected in first criterion of selection
+		List<NodeResidentModel> residentNodeList = mfragSelected.getNodeResidentList();
+		
+//		boolean isResident = false;
+		for (int i = 0; i < residentNodeList.size(); i++) {
+			
+			// Resident node is random variable related to an attribute or relationship.
+			if (residentNodeList.get(i).getEventVariable().getClass()
+					== RelationshipModel.class) {
+				
+				NodeResidentModel residentNode = residentNodeList.get(i);
+				RelationshipModel relationshipModel = (RelationshipModel)residentNode.getEventVariable();
+				
+				// Can exist more than one cause with same name, but with different arguments.
+				if (relationshipModel.equals(cause.getRelationshipModel())) {
+					return residentNode;
+				}
+			}
+		}
+		return null;
+			
+	}
+	
+	/**
+	 * Verify if cause relationship defined in rule was mapped as 
+	 * input node in MFrag related to rule. If it is, then return
+	 * input node identified.
+	 * @param cause
+	 * @return inputNode
+	 */
+	public NodeInputModel searchInputNode(CauseVariableModel cause) {
+		
+		List<NodeInputModel> inputNodeList = mfragSelected.getNodeInputList();
+		
+		for (int i = 0; i < inputNodeList.size(); i++) {
+			
+			// Input node is random variable related to an attribute or relationship.
+			if (inputNodeList.get(i).getEventVariable().getClass()
+					== RelationshipModel.class) {
+				
+				NodeInputModel inputNode = inputNodeList.get(i);
+				RelationshipModel relationshipModel = (RelationshipModel)inputNode.getEventVariable();
+				
+				if (relationshipModel.equals(cause.getRelationshipModel())) {
+					return inputNode;
+				}
+			}
+		}
+		return null;
+			
+	}
+	
+	/**
+	 * Select MFrag related to group.
+	 */
+	public void selectMFragRelated() {
+		List<MFragModel> mfrag = generateMTheoryController.getMTheory().getMFragList();
+		mfragSelected = null;
+		
+		for (int i = 0; i < mfrag.size(); i++) {
+			if (mfrag.get(i).getId().equals(group.getId())) {
+				mfragSelected = mfrag.get(i);
+				break;
+			}
+		}
+	}
+}
+
---
+++ UMPST/src/main/java/unbbayes/model/umpst/implementation/algorithm/SecondCriterionOfSelection.java
@@ -0,0 +1,392 @@
+package unbbayes.model.umpst.implementation.algorithm;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import unbbayes.controller.umpst.GenerateMTheoryController;
+import unbbayes.model.umpst.ObjectModel;
+import unbbayes.model.umpst.entity.EntityModel;
+import unbbayes.model.umpst.group.GroupModel;
+import unbbayes.model.umpst.implementation.CauseVariableModel;
+import unbbayes.model.umpst.implementation.EffectVariableModel;
+import unbbayes.model.umpst.implementation.NecessaryConditionVariableModel;
+import unbbayes.model.umpst.implementation.OrdinaryVariableModel;
+import unbbayes.model.umpst.implementation.node.NodeContextModel;
+import unbbayes.model.umpst.implementation.node.NodeInputModel;
+import unbbayes.model.umpst.implementation.node.NodeObjectModel;
+import unbbayes.model.umpst.implementation.node.NodeType;
+import unbbayes.model.umpst.project.UMPSTProject;
+import unbbayes.model.umpst.rule.RuleModel;
+import unbbayes.util.ArrayMap;
+
+/**
+ * This criterion classifies not defined nodes present in MFrags in context, input
+ * or resident nodes and set the dependencies between each other.  
+ * @author Diego Marques
+ */
+public class SecondCriterionOfSelection {
+	
+	private UMPSTProject umpstProject;
+	private GenerateMTheoryController generateMTheoryController;
+	
+	private Map<String, GroupModel> mapGroup;
+	private Map<String, RuleModel> mapRule;
+	private Map<String, NodeObjectModel> mapDoubtNodes;
+	private List<ObjectModel> objectModel;
+	
+	public SecondCriterionOfSelection(UMPSTProject umpstProject,
+			GenerateMTheoryController generateMTheoryController) {
+		
+		this.umpstProject = umpstProject;
+		this.generateMTheoryController = generateMTheoryController;
+		
+		mapGroup = new HashMap<String, GroupModel>();
+		mapRule = new HashMap<String, RuleModel>();
+		mapDoubtNodes = new HashMap<String, NodeObjectModel>();
+		
+		mainSelection();		
+	}
+	
+	/**
+	 * Main Selection.
+	 */
+	public void mainSelection() {
+		List<RuleModel> listRules = new ArrayList<RuleModel>();
+		DefineMFragRelation mfragRelation;
+		
+		mapGroup = umpstProject.getMapGroups();
+		Set<String> keys = mapGroup.keySet();
+		TreeSet<String> sortedKeys = new TreeSet<String>(keys);
+		
+		for (String key : sortedKeys) {
+			GroupModel group = mapGroup.get(key);
+			
+			listRules = group.getBacktrackingRules();
+			for (int i = 0; i < listRules.size(); i++) {
+				
+				RuleModel rule = group.getBacktrackingRules().get(i);
+				if (compareElements(rule, group)) {
+					
+					insertContextNode(rule, group);
+					mfragRelation = new DefineMFragRelation(rule, group, generateMTheoryController,
+							umpstProject, this);
+//					searchOVMissing(rule, group);
+//					defineMFragCausal(rule, group);
+					
+				} else {
+					System.err.println("Number of element in rule: " + rule.getId() +
+							" " + "does not match with group: " + group.getId());
+					
+					// Elements of rule that does not check
+					for (int j = 0; j < objectModel.size(); j++) {
+						System.err.println(objectModel.get(j).getName());
+					}
+				}
+			}
+			if (group.getBacktrackingRules().size() == 0) {
+				
+				// Add missing OrdinaryVariables
+//				insertMissingOV(group);
+			}
+		}
+	}
+	
+	public void classifyInputNode(RuleModel rule, GroupModel group) {
+		
+		List<RuleModel> causeIsEffectList = new ArrayList<RuleModel>();
+		
+		mapRule = umpstProject.getMapRules();
+		Set<String> keys = mapRule.keySet();
+		TreeSet<String> sortedKeys = new TreeSet<String>(keys);
+				
+		int flag = 0;
+		for (String key : sortedKeys) {
+			RuleModel ruleSelected = mapRule.get(key);
+			if (!(ruleSelected.getId().equals(rule.getId()))) {
+				
+				for (int k = 0; k < rule.getCauseVariableList().size(); k++) {
+					CauseVariableModel cause = rule.getCauseVariableList().get(k);
+				
+					for (int j = 0; j < ruleSelected.getEffectVariableList().size(); j++) {	
+						EffectVariableModel effect = ruleSelected.getEffectVariableList().get(j);
+						
+						if (cause.getRelationshipModel().equals(effect.getRelationshipModel())) {
+							// cause is effect
+							causeIsEffectList.add(ruleSelected);
+							flag++;
+						}
+					}
+				}
+			}
+		}
+		
+		if (causeIsEffectList.size() > 0) {
+			mapRule = umpstProject.getMapRules();
+			Set<String> keys2 = mapRule.keySet();
+			TreeSet<String> sortedKeys2 = new TreeSet<String>(keys2);
+					
+			for (String key2 : sortedKeys2) {
+				RuleModel ruleSelected = mapRule.get(key2);
+			
+				for (int i = 0; i < causeIsEffectList.size(); i++) {
+					String idCauseIsEffect = causeIsEffectList.get(i).getId();
+					
+					if (!(ruleSelected.getId().equals(idCauseIsEffect))) {
+						
+						for (int k = 0; k < ruleSelected.getCauseVariableList().size(); k++) {
+							CauseVariableModel cause = ruleSelected.getCauseVariableList().get(k);
+							String id = cause.getId();
+							
+							String sentence = cause.getRelationship() + "(";
+							for (int k1 = 0; k1 < cause.getArgumentList().size(); k1++) {				
+								sentence = sentence + cause.getArgumentList().get(k1) + ", ";
+							}
+							int index = sentence.lastIndexOf(", ");
+							sentence = sentence.substring(0, index);
+							sentence = sentence + ")";
+							
+							NodeInputModel inputNode = new NodeInputModel(id, sentence, NodeType.INPUT, cause);
+							
+							List<GroupModel> idGroupList = ruleSelected.getFowardtrackingGroupList();
+							if (idGroupList.size() == 1) {
+								generateMTheoryController.addInputNodeInMFrag(idGroupList.get(0).getId(), inputNode);
+							} else {
+								System.err.println("Error classifyInputNode. Rule is in more than one group.");
+							}
+						}				
+					}
+				}
+			}
+		}
+	}
+	
+	public void setOthersResidentNode() {
+		
+//		// Set resident node as effect event present in rule related to group.
+//		for (int i = 0; i < rule.getEffectVariableList().size(); i++) {
+//			
+//			EffectVariableModel effect = rule.getEffectVariableList().get(i);
+//			if (effect.getRelationship() != null) {
+//				
+//				String id = effect.getId();
+//				String sentence = effect.getRelationship() + "(";
+//				for (int j = 0; j < effect.getArgumentList().size(); j++) {				
+//					sentence = sentence + effect.getArgumentList().get(j) + ", ";
+//				}
+//				int index = sentence.lastIndexOf(", ");
+//				sentence = sentence.substring(0, index);
+//				sentence = sentence + ")";
+//			
+//				NodeResidentModel residentNode = new NodeResidentModel(id, sentence,
+//						NodeType.RESIDENT, effect);
+//				
+//				generateMTheoryController.addNodeResidentInMFrag(group.getId(), residentNode);
+//				
+//			} else {
+//				System.err.println("Error EffectModel, RULE: "+ rule.getId() + 
+//						". Effect relationship not define");
+//			}
+//			
+//		}
+	}
+	
+	/**
+	 * Insert missing Ordinary Variables as context node in groups that
+	 * do not have definition rule.
+	 */
+	public void insertMissingOV(GroupModel group) {
+		OrdinaryVariableModel ov;
+		
+		for (int i = 0; i < group.getBacktrackingEntities().size(); i++) {
+			EntityModel entity = group.getBacktrackingEntities().get(i);
+			
+			String id = Integer.toString(i);
+			String variable = entity.getName().toLowerCase();
+			String typeEntity = entity.getName();
+			
+			ov = new OrdinaryVariableModel(id, variable, typeEntity, entity);
+			
+			String name = "isA( " + variable + ", " + typeEntity + " )" ;
+			
+			NodeContextModel contextNode = new NodeContextModel(
+					ov.getId(), name, NodeType.CONTEXT, ov);
+			
+			generateMTheoryController.addContextNodeInMFrag(group.getId(), contextNode);
+		}
+	}
+	
+	/**
+	 * Insert missing Ordinary Variables that has rule, but it is not
+	 * present.
+	 * @param rule
+	 * @param model
+	 */
+	public void searchOVMissing(RuleModel rule, GroupModel group) {
+		
+		int flag = 0;
+		for (int j = 0; j < group.getBacktrackingEntities().size(); j++) {
+			for (int i = 0; i < rule.getOrdinaryVariableList().size(); i++) {
+				EntityModel entityOV = rule.getOrdinaryVariableList().get(i).getEntityObject();
+				EntityModel entityGroup = group.getBacktrackingEntities().get(j);
+				
+				if (entityOV.getId().equals(entityGroup.getId())) {
+					flag++;
+					break;
+				}
+			}
+		}
+		if (flag < group.getBacktrackingEntities().size()) {
+			System.err.println("Error OV definition" + "Missing Entities GROUP :" + group.getId() +
+					"Missing Entities RULE :" + rule.getId());
+		}
+	}
+	
+	public void insertContextNode(RuleModel rule, GroupModel group) {
+		
+		// Add OrdinaryVariable as context node.
+		for (int i = 0; i < rule.getOrdinaryVariableList().size(); i++) {
+			OrdinaryVariableModel ov = rule.getOrdinaryVariableList().get(i);
+			String id = ov.getId();		
+			String name = "isA( " + ov.getVariable() + ", " + ov.getTypeEntity() + " )" ;
+			
+			NodeContextModel contextNode = new NodeContextModel(id, name, NodeType.CONTEXT, ov);
+			generateMTheoryController.addContextNodeInMFrag(group.getId(), contextNode);
+		}
+		
+		// Add NecessaryCondtion as context node.
+		for (int i = 0; i < rule.getNecessaryConditionList().size(); i++) {
+			NecessaryConditionVariableModel nc = rule.getNecessaryConditionList().get(i);
+			String id = nc.getId();
+			String name = nc.getFormula();
+			
+			NodeContextModel contextNode = new NodeContextModel(id, name, NodeType.CONTEXT, nc);
+			generateMTheoryController.addContextNodeInMFrag(group.getId(), contextNode);
+		}		
+	}
+	
+	/**
+	 * Compare if rule is according to group related.
+	 * @param rule
+	 * @param group
+	 * @return
+	 */
+	public boolean compareElements(RuleModel rule, GroupModel group) {
+		int confirm = 0;
+		int actflag = 0;
+		int lstflag = 0;
+		
+		objectModel = new ArrayList<ObjectModel>();
+		
+		// Attributes
+		for (int i = 0; i < rule.getAttributeList().size(); i++) {
+			for (int j = 0; j < group.getBacktrackingAtributes().size(); j++) {
+				String idRuleAtt = rule.getAttributeList().get(i).getId();
+				String idGroupAtt = group.getBacktrackingAtributes().get(j).getId();
+				if (idRuleAtt.equals(idGroupAtt)) {
+					actflag++;
+					lstflag = actflag;
+					break;
+				} else {
+					lstflag++;
+				}
+			}
+			if (lstflag > actflag) {
+				objectModel.add(rule.getAttributeList().get(i));
+			}
+		}
+		if (actflag >= rule.getAttributeList().size()) {
+			confirm++;
+		}
+		
+		// Entities
+		actflag = 0;
+		lstflag = 0;
+		for (int i = 0; i < rule.getEntityList().size(); i++) {
+			for (int j = 0; j < group.getBacktrackingEntities().size(); j++) {
+				String idRuleEnt = rule.getEntityList().get(i).getId();
+				String idGroupEnt = group.getBacktrackingEntities().get(j).getId();
+				if (idRuleEnt.equals(idGroupEnt)) {
+					actflag++;
+					lstflag = actflag;
+					break;
+				} else {
+					lstflag++;
+				}
+			}		
+			if (lstflag > actflag) {
+				objectModel.add(rule.getEntityList().get(i));
+			}
+		}
+		if (actflag >= rule.getEntityList().size()) {
+			confirm++;
+		}
+		
+		// Relationship
+		actflag = 0;
+		lstflag = 0;
+		for (int i = 0; i < rule.getRelationshipList().size(); i++) {
+			for (int j = 0; j < group.getBacktrackingRelationship().size(); j++) {
+				String idRuleRel = rule.getRelationshipList().get(i).getId();
+				String idGroupRel = group.getBacktrackingRelationship().get(j).getId();
+				if (idRuleRel.equals(idGroupRel)) {
+					actflag++;
+					lstflag = actflag;
+					break;
+				} else {
+					lstflag++;
+				}
+			}
+			if (lstflag > actflag) {
+				objectModel.add(rule.getRelationshipList().get(i));
+			}
+		}
+		if (actflag >= rule.getRelationshipList().size()) {
+			confirm++;
+		}
+		
+		if (confirm == 3) {
+			return true;
+		}
+		
+		return false;		
+	}
+	
+	/**
+	 * Set ID according to the last necessary condition ID created.
+	 */
+	public String getOVId(RuleModel rule) {
+		int id = 0;
+		int greaterID = -1;
+		boolean beginID = true; // created to set Id = 0
+		for (int i = 0; i < rule.getOrdinaryVariableList().size(); i++) {
+			if (greaterID < Integer.parseInt(rule.getOrdinaryVariableList().get(i).getId())) {
+				greaterID = Integer.parseInt(rule.getOrdinaryVariableList().get(i).getId());
+				beginID = false;
+			}
+		}
+		if (!beginID) {
+			id = greaterID+1;
+			return Integer.toString(id);
+		}
+		return "0";
+	}
+
+	/**
+	 * @return the mapDoubtNodes
+	 */
+	public Map<String, NodeObjectModel> getMapDoubtNodes() {
+		return mapDoubtNodes;
+	}
+
+	/**
+	 * @param mapDoubtNodes the mapDoubtNodes to set
+	 */
+	public void setMapDoubtNodes(Map<String, NodeObjectModel> mapDoubtNodes) {
+		this.mapDoubtNodes = mapDoubtNodes;
+	}
+
+}
